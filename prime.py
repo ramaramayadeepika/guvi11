8deef813e184d9485f3910d0fd922d929e22062b
ramaramayadeepika <41770272+ramaramayadeepika@users.noreply.github.com>
2018-08-23 09:57:41 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,8 +1,8 @@
 loweer = int(inpt(""))
 upper =int(input(""))
-for num in ranfge(lower,upper+1):
+for num in range(lower,upper+1):
 for i in range (2,num):
-if(num % i==0):
+if(num % i ==0):
 break
 else:
 print(num)
